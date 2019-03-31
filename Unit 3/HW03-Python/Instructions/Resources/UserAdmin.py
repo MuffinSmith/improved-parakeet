707f43e2a840f07823f44199cef9ccf430c52a8c
MuffinSmith <grantross64@gmail.com>
2019-03-31 10:42:00 -0700

    Unit 3 Homework

--- Unit 3/HW03-Python/Instructions/Resources/UserAdmin.py
+++ Unit 3/HW03-Python/Instructions/Resources/UserAdmin.py
@@ -11,3 +11,20 @@ adminList = [
 ]
 
 # Build your login functions below
+def getCreds():
+    userInfo = {}
+    userInfo["username"] = input("Enter user name: ")
+    userInfo["password"] = input("Enter password: ")
+    return userInfo
+
+def checkLogin(userInfo, adminList):
+    for x in adminList:
+        if(x["username"] == userInfo["username"]):
+            if(x["password"] == userInfo["password"]):
+                print("YOU HAVE LOGGED IN!")
+                return False
+    return True        
+
+while  checkLogin(getCreds(),adminList):
+    print("----------")
+
