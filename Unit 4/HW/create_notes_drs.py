66dece404f165dde8d821c55f5fa99644205c699
Grant <muffin@localhost.localdomain>
2019-04-06 12:22:21 -0700

    small fix

--- Unit 4/HW/create_notes_drs.py
+++ Unit 4/HW/create_notes_drs.py
@@ -2,12 +2,15 @@ import os
 
 def main():
     try:
+        working_dir = os.path.join(os.getcwd(), "CyberSecurity-Notes")
         for x in range(1,25):
 
             folder_name ="week "+ str(x)
             for y in range(1,4):
                 nested_folder = "day " + str(y)
-                os.makedirs(os.path.join(os.path.join(os.getcwd(), folder_name), nested_folder ))
-    except:
-        print("Folders already exist")
-main()
+                os.makedirs(os.path.join(os.path.join(working_dir, folder_name), nested_folder ))
+    except OSError as error:
+        print(str(error))
+
+if __name__ == "__main__":        
+    main()
